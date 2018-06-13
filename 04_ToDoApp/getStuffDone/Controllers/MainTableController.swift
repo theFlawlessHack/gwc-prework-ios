15da5dc6778025abfa38bbf9753bd1023b57705d
Jessica Joseph <jessjeanjo@gmail.com>
2018-06-13 14:29:19 -0400

    add swipe to delete, finish project

--- 04_ToDoApp/getStuffDone/Controllers/MainTableController.swift
+++ 04_ToDoApp/getStuffDone/Controllers/MainTableController.swift
@@ -1,48 +1,69 @@
-//
-//  MainTableController.swift
-//  getStuffDone
-//
 //  Created by Jessica Joseph on 6/13/18.
 //  Copyright © 2018 TFH. All rights reserved.
-//
 
+import CoreData
 import UIKit
 
 class MainTableController: UITableViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // self.clearsSelectionOnViewWillAppear = false
-        self.navigationItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        getData()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return tasks.count
+    }
+    
+    func getData() {
+        do {
+            tasks = try context.fetch(Task.fetchRequest())
+            print(tasks)
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        } catch {
+            print("Couldn't fetch Data")
+        }
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row].taskName
 
         return cell
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let delete = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            let task = self.tasks[indexPath.row]
+            self.context.delete(task)
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            self.tasks.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        return [delete]
+    }
+    
     /*
+     
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
